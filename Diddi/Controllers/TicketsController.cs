ac1dbdd7091701048583ecb71e292c65dc65af3f
killpathhh <jenya.sofronoff@gmail.com>
2024-11-25 23:20:15 +0300

    Добавьте файлы проекта.

---
+++ Diddi/Controllers/TicketsController.cs
@@ -0,0 +1,31 @@
+﻿using Diddi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diddi.Controllers
+{
+    [ApiController]
+    [Route("api/tickets")]
+    public class TicketsController : ControllerBase
+    {
+        private static List<Ticket> Tickets = new();
+
+        [HttpGet]
+        public IActionResult GetTickets()
+        {
+            return Ok(Tickets);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTicket([FromBody] Ticket newTicket)
+        {
+            if (string.IsNullOrEmpty(newTicket.Name) || string.IsNullOrEmpty(newTicket.Description))
+            {
+                return BadRequest("Title and Description are required.");
+            }
+
+            Tickets.Add(newTicket);
+            return Ok(newTicket);
+        }
+
+    }
+}
---
+++ Diddi/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace Diddi.Models
+{
+    public class Ticket
+    {
+
+        public string ?Name { get; set; }
+        public string ?Description { get; set; }
+
+    }
+}
